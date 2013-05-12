a83e21cd27baddd83ff72e826a6f8b9420486ef8
MobiApp121 <info@cutproductions.com>
2013-05-12 15:20:50 +0200

    Change Authors for Author(s)to avoid singular/plural issue

--- themes-book/pressbooks-book/page-authors.php
+++ themes-book/pressbooks-book/page-authors.php
@@ -3,7 +3,7 @@ get_header();
 if (get_option('blog_public') == '1' || (get_option('blog_public') == '0' && current_user_can_for_blog($blog_id, 'read'))):
 if (have_posts()) the_post(); ?>
 			<div id="post-<?php the_ID(); ?>" <?php post_class('author-block-wrap'); ?>>
-				<h2 class="page-title"><?php _e('Authors', 'pressbooks'); ?></h2>
+				<h2 class="page-title"><?php _e('Author(s)', 'pressbooks'); ?></h2>
 				
 				<?php
 			  $authors = get_posts(array('post_type' => 'back-matter',
