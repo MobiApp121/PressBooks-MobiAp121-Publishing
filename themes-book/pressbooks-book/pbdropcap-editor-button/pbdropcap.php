649826d7b0c90978f16014abb1e17af87db3de27
MobiApp121 <info@cutproductions.com>
2013-05-12 14:38:34 +0200

    Create pbdropcap.php
    New Drop Cap plug-in

---
+++ themes-book/pressbooks-book/pbdropcap-editor-button/pbdropcap.php
@@ -0,0 +1,15 @@
+<?php
+add_action( 'init', 'pbdropcap_button' );
+function pbdropcap_button() {
+  add_filter("mce_external_plugins", "pbdropcap_add_button");
+    add_filter('mce_buttons', 'pbdropcap_register_button');
+}	
+function pbdropcap_add_button($plugin_array) {
+	$plugin_array['pbdropcap'] = get_template_directory_uri() . '/pbdropcap-editor-button/pbdropcap-plugin.js';
+	return $plugin_array;
+}
+function pbdropcap_register_button($button) {
+	array_push( $button, 'dropcap' ); // dropcap'
+	return $button;
+}
+?>
